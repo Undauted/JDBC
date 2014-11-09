a5e23ad3410310b05e6d3cb24aff6b78b76d5017
Undauted <piotrkacprowicz92@gmail.com>
2014-11-09 13:13:25 +0100

    Update Testy.java

--- src/test/java/com/example/jdbcdemo/service/Testy.java
+++ src/test/java/com/example/jdbcdemo/service/Testy.java
@@ -18,15 +18,14 @@ public class Testy {
 	PrawnikManager prawnikManager = new PrawnikManager();
 	
 	
-	
 	private final static String IMIE_1 = "Zygmunt";
-	private final static String IMIE_2 = "Stefan";
+	//private final static String IMIE_2 = "Stefan";
 	private final static String NAZWISKO_1 = "Tak";
 	private final static int WIEK_1 = 34;
 	private final static int NUMER = 34;
 	private final static String ULICA = "Grunwaldzka";
 	private final static String MIEJSCOWOSC = "Gdańsk";
-	private final static String ULICA2 = "Kołobrzeska";
+	//private final static String ULICA2 = "Kołobrzeska";
 	
 //---------------------------Sprawdzenie połączenia prawników---------------------------------------------------------	
 	@Test
@@ -42,34 +41,37 @@ public class Testy {
 	@Test
 	public void sprawdzDodawanieAdresow(){
 		
-		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
-		
 		prawnikManager.usunWszystkich();
 		adresManager.usunWszystkich();
+		
+		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
 		
 		assertEquals(1,adresManager.zapisz(adres));
 		
 		List<Adres> adresy = adresManager.pobierzWszystkie();
 		Adres adresyretrived = adresy.get(0);
 		
-		
+		assertEquals(MIEJSCOWOSC, adresyretrived.getMiejscowosc());
 		assertEquals(ULICA, adresyretrived.getUlica());
 		assertEquals(NUMER, adresyretrived.getNr());
-		
-		
-		
-	}
+		}
 //---------------------------Dodanie prawników---------------------------------------------------------	
 	@Test
 	public void sprawdzDodawaniePrawnik(){
 
+		prawnikManager.usunWszystkich();
+		adresManager.usunWszystkich();
+		
+		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
+		
+		assertEquals(1,adresManager.zapisz(adres));
+		
 		List<Adres> adresy = adresManager.pobierzWszystkie();
 		Adres adre = adresy.get(0);
 		
 		Prawnik prawnik = new Prawnik(IMIE_1,NAZWISKO_1,WIEK_1);
 		prawnik.setAdres(adre);
 		
-		prawnikManager.usunWszystkich();
 		assertEquals(1,prawnikManager.zapisz(prawnik));
 		
 		List<Prawnik> prawnicy = prawnikManager.pobierzWszystkich();
@@ -91,7 +93,6 @@ public class Testy {
 		//Dodanie adresu i prawnika do bazy
 		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
 		
-		
 		assertEquals(1,adresManager.zapisz(adres));
 		
 		List<Adres> adresy = adresManager.pobierzWszystkie();
@@ -110,62 +111,64 @@ public class Testy {
 		assertEquals(IMIE_1,pra.getImie());
 		assertEquals(NAZWISKO_1,pra.getNazwisko());
 		assertEquals(WIEK_1,pra.getWiek());
+		assertEquals(adre.getId(),pra.getAdres().getId());
 		
 	}
 //---------------------------Pobranie selectem adresów---------------------------------------------------------	
 	@Test
-	public void sprawdzSelectWszystkiego()
+	public void sprawdzSelectWszystkiechAdresów()
 	{
+		prawnikManager.usunWszystkich();
+		adresManager.usunWszystkich();
+		
+		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
+		assertEquals(1,adresManager.zapisz(adres));
+		
 		List<Adres> adresy = adresManager.pobierzWszystkie();
 		Adres adr = adresy.get(0);
-			
+		
 		assertNotNull(adr);
-	}
-//---------------------------Usuwanie wszystkich prawników---------------------------------------------------------	
-	@Test
-	public void sprawdzUsuwawniewszystkichPrawnikow() 
-	{	
-		prawnikManager.usunWszystkich();
-		adresManager.usunWszystkich();
-		
-		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
-		
-		
-		assertEquals(1,adresManager.zapisz(adres));
-		
-		List<Adres> adresy = adresManager.pobierzWszystkie();
-		Adres adre = adresy.get(0);
-		
-		Prawnik prawnik = new Prawnik(IMIE_1,NAZWISKO_1,WIEK_1);
-		prawnik.setAdres(adre);
-		
-		
-		assertEquals(1,prawnikManager.zapisz(prawnik));
-		int liczba = prawnikManager.usunWszystkich();
-		assertEquals(1, liczba);
+		assertEquals(adr.getMiejscowosc(),MIEJSCOWOSC);
+		assertEquals(adr.getUlica(),ULICA);
+		assertEquals(adr.getNr(),NUMER);
 		
 	}
-//---------------------------Usuwanie wszystkich adresów---------------------------------------------------------
-	@Test
-	public void sprawdzUsuwawniewszystkiAdresy() 
-	{	
-		prawnikManager.usunWszystkich();
-		adresManager.usunWszystkich();
-		
-		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
-		
-		
-		assertEquals(1,adresManager.zapisz(adres));
-		
-		
-		
-		int liczba = adresManager.usunWszystkich();
-		assertEquals(1, liczba);
-	}
+//---------------------------Pobranie selectem prawników---------------------------------------------------------	
+		@Test
+		public void sprawdzSelectWszystkichPrawników()
+		{
+			prawnikManager.usunWszystkich();
+			adresManager.usunWszystkich();
+			
+			Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
+			
+			assertEquals(1,adresManager.zapisz(adres));
+			
+			List<Adres> adresy = adresManager.pobierzWszystkie();
+			Adres adre = adresy.get(0);
+			
+			Prawnik prawnik = new Prawnik(IMIE_1,NAZWISKO_1,WIEK_1);
+			prawnik.setAdres(adre);
+			
+			assertEquals(1,prawnikManager.zapisz(prawnik));
+			
+			List<Prawnik> praw = prawnikManager.pobierzWszystkich();
+			Prawnik pra = praw.get(0);
+			
+			assertNotNull(pra);
+			assertEquals(IMIE_1, pra.getImie());
+			assertEquals(NAZWISKO_1, pra.getNazwisko());
+			assertEquals(WIEK_1, pra.getWiek());
+			assertEquals(adre.getId(), pra.getAdres().getId());
+			
+			
+		}
 //---------------------------Usuwanie pojedynczego rekordu---------------------------------------------------------
 	@Test
 	public void sprawdzUsuwanie() throws Exception
 	{
+		prawnikManager.usunWszystkich();
+		adresManager.usunWszystkich();
 		
 		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
 		
@@ -181,15 +184,12 @@ public class Testy {
 		
 		assertEquals(1,prawnikManager.zapisz(prawnik));
 	
-		List<Prawnik> praw = prawnikManager.pobierzWszystkich();
-		Prawnik pra = praw.get(0);
 		
-		
-		assertEquals(1,prawnikManager.usun(pra));
 		assertEquals(1,adresManager.usun(adre));
 		
-		assertEquals(0,adresManager.pobierzWszystkichAdresy(adre).size());
-		
+		assertEquals(0,adresManager.pobierzWszystkie().size());
+		assertEquals(0,prawnikManager.pobierzWszystkich().size());
+	
 		
 		
 	}
@@ -220,38 +220,8 @@ public class Testy {
 		
 		assertEquals(1, prawnikManager.editPrawnik(adre,pra));
 		
-		
+		assertEquals(adre.getId(),pra.getAdres().getId());
 		
 	}
-//---------------------------Edycja adresów---------------------------------------------------------
-	@Test
-	public void sprawdzeditAdresow()
-	{
-		
-		
-		
-		adresManager.usunWszystkich();
-		Adres adres = new Adres(MIEJSCOWOSC,ULICA,NUMER);
-		
-		
-		assertEquals(1,adresManager.zapisz(adres));
-		
-		List<Adres> adresy = adresManager.pobierzWszystkie();
-		Adres adre = adresy.get(0);
-		
-		Prawnik prawnik = new Prawnik(IMIE_1,NAZWISKO_1,WIEK_1);
-		prawnik.setAdres(adre);
-		
-		
-		assertEquals(1,prawnikManager.zapisz(prawnik));
-		
-		assertEquals(1, adresManager.editPrawnik(ULICA2,adre));
-		
-		List<Adres> adresy2 = adresManager.pobierzWszystkie();
-		Adres adr = adresy2.get(0);
-		
-		assertEquals(ULICA2,adr.getUlica());
-		}
-
 
 }
